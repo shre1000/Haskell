b90d63e4d0ff316efa060b4fe363e783762bc3ea
Shreyasi <31860419+shre1000@users.noreply.github.com>
2018-05-17 21:12:48 -0500

    Create IndexingFile3Test.hs

---
+++ IndexingFile3Test.hs
@@ -0,0 +1,10 @@
+
+-- This is test case module for File3.hs
+
+module Test3
+where
+import File3
+
+main = 
+    do 
+		makeIndex "this is the test case.\n\nI am developing Code to write and modify some code for original functions.\n"
